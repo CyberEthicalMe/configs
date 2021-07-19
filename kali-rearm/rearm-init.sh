f7e0c8b13e3002279fb586ea5acaf392a9fc42d3
Kamil Gierach-Pacanek <20944885+KamilPacanek@users.noreply.github.com>
2021-07-20 00:10:31 +0200

    Update rearm-init.sh

--- kali-rearm/rearm-init.sh
+++ kali-rearm/rearm-init.sh
@@ -1,2 +1,2 @@
-sudo apt install $(awk '{print $1'} tools.list)
+sudo apt install $(cat tools.list)
 sudo gem install evil-winrm # https://github.com/Hackplayers/evil-winrm#installation--quick-start-4-methods

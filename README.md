d704aa93d4f6dd1bd4a86ddf1900a63fb4dc2941
Kamil Gierach-Pacanek <20944885+KamilPacanek@users.noreply.github.com>
2022-02-14 16:23:19 +0100

    Migration to CyberEthical repository

--- README.md
+++ README.md
@@ -13,4 +13,4 @@ Various configuration files to keep environments as homogeneous as possible acro
 
 *  [`git-gpg.asc`](./git-gpg.asc)
 
-Used for signing Git commits. [More details here](https://gist.github.com/KamilPacanek/390e46e182eb008c0051828e562e8d2a).
+Used for signing Git commits. [More details here](https://blog.cyberethical.me/how-to-set-up-git-commit-signing).
--- htb-pwnbox/init-pwnbox.sh
+++ htb-pwnbox/init-pwnbox.sh
@@ -6,7 +6,7 @@ WDIR="$HOME/my_data"
 cd $WDIR
 
 # get prefonfigured user_init file
-wget https://raw.githubusercontent.com/KamilPacanek/configs/master/htb-pwnbox/user_init --backups=1
+wget https://raw.githubusercontent.com/CyberEthicalMe/configs/master/htb-pwnbox/user_init --backups=1
 
 # get Powerline font for tmux theme
 mkdir -p fonts
@@ -15,8 +15,8 @@ wget https://github.com/powerline/fonts/raw/master/Cousine/Cousine%20for%20Power
 # prepare home directory to preload in user_init
 mkdir -p home
 cd home
-wget https://raw.githubusercontent.com/KamilPacanek/configs/master/kali-rearm/.bash_aliases
-wget https://raw.githubusercontent.com/KamilPacanek/configs/master/tmux/.tmux.conf
+wget https://raw.githubusercontent.com/CyberEthicalMe/configs/master/kali-rearm/.bash_aliases
+wget https://raw.githubusercontent.com/CyberEthicalMe/configs/master/tmux/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm .tmux/plugins/tpm
 
 # create RSA keypair for persistence over SSH
@@ -25,7 +25,7 @@ ssh-keygen -t rsa -f .ssh/id_rsa
 
 # get terminal settings export script
 cd $WDIR
-wget https://raw.githubusercontent.com/KamilPacanek/configs/master/htb-pwnbox/export-mate-terminal.sh
+wget https://raw.githubusercontent.com/CyberEthicalMe/configs/master/htb-pwnbox/export-mate-terminal.sh
 
 # get some tools
 git clone https://github.com/ffuf/ffuf $WDIR/tools/ffuf ; cd $WDIR/tools/ffuf ; go get ; go build
